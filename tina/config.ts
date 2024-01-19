6b5429b20e20ba659c025007be1d1908e6a5487e
ashpatil07 <ashpatil07@gmail.com>
2024-01-20 02:17:59 +0530

    tinacms

---
+++ tina/config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: 'c6e8d19e-cc09-4ce4-a70e-6ac1c0eda3e2',
+  // Get this from tina.io
+  token: 'e057908d7e82066d5f682a28eb24154813cae5ee',
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "/",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "assets",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "_posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
